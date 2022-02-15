d8f54218cab7001ef757e96c803987b5d8ba7f1a
DionysusIV <70836810+DionysusIV@users.noreply.github.com>
2022-02-15 18:03:32 +0300

    Add files via upload

---
+++ src/main/java/hw/Stage.java
@@ -0,0 +1,13 @@
+package hw;
+
+import hw.Car;
+
+public abstract class Stage {
+    protected int length;
+    protected String description;
+    public String getDescription() {
+        return description;
+    }
+    public abstract void go(Car c);
+}
+

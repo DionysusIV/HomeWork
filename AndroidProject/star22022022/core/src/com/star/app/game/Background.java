9d48c0d23d7a9f1c20bd1b0bff59ac0729534524
DionysusIV <70836810+DionysusIV@users.noreply.github.com>
2022-03-04 19:36:56 +0300

    Add files via upload

---
+++ AndroidProject/star22022022/core/src/com/star/app/game/Background.java
@@ -0,0 +1,71 @@
+package com.star.app.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.star.app.StarGame;
+
+import static com.star.app.screen.ScreenManager.*;
+
+public class Background {
+    private class Star {
+        private Vector2 position;
+        private Vector2 velocity;
+        private float scale;
+
+        public Star() {
+            this.position = new Vector2(MathUtils.random(0, SCREEN_WIDTH),
+                    MathUtils.random(-200, SCREEN_HEIGHT + 200));
+            this.velocity = new Vector2(MathUtils.random(-40, -5), 0);
+            scale = Math.abs(velocity.x / 40f) * 0.8f;
+        }
+
+        public void update(float dt) {
+            position.x += (velocity.x - gameController.getHero().getVelocity().x * 0.1f) * dt;
+            position.y += (velocity.y - gameController.getHero().getVelocity().y * 0.1f) * dt;
+
+            if (position.x < -200) {
+                position.x = SCREEN_WIDTH + 200;
+                position.y = MathUtils.random(0, SCREEN_HEIGHT);
+                scale = Math.abs(velocity.x / 40f) * 0.8f;
+            }
+        }
+    }
+
+    private final int STAR_COUNT = 600;
+    private Texture textureCosmos;
+    private Texture textureStar;
+    private GameController gameController;
+    private Star[] stars;
+
+    public Background(GameController gameController) {
+        this.gameController = new GameController();
+        this.textureCosmos = new Texture("bg.png");
+        this.textureStar = new Texture("star16.png");
+        this.stars = new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star();
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(textureCosmos, 0, 0);
+        for (int i = 0; i < stars.length; i++) {
+            batch.draw(textureStar, stars[i].position.x - 8, stars[i].position.y - 8,
+                    8,8,16,16, stars[i].scale, stars[i].scale,
+                    0,0,0,16,16,false,false);
+            if (MathUtils.random(300) < 1){
+                batch.draw(textureStar, stars[i].position.x - 8, stars[i].position.y - 8,
+                        8,8,16,16, stars[i].scale * 2, stars[i].scale * 2,
+                        0,0,0,16,16,false,false);
+            }
+        }
+    }
+
+    public void update(float dt) {
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].update(dt);
+        }
+    }
+}
